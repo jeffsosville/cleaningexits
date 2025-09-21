3bad9148277f300387b98358a34dbf1aefbeb678
jeffsosville <148246966+jeffsosville@users.noreply.github.com>
2025-09-21 17:55:55 -0400

    Create listings.ts

---
+++ types/listings.ts
@@ -0,0 +1,20 @@
+// types/listings.ts
+export interface Listing {
+  header: string;
+  location: string;
+  price: number;
+  description: string;
+  img: string;
+  listNumber: number;
+  urlStub: string;
+  cashFlow: string;
+  ebitda: string;
+  brokerCompany: string;
+  brokerContactFullName: string;
+  region: string;
+  hotProperty: string;
+  recentlyUpdated: string;
+  recentlyAdded: string;
+  scraped_at: string;
+  surrogate_key: string;
+}
