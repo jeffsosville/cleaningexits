6c12157133b0cd087e410e78a221f2c398fe7428
jeffsosville <148246966+jeffsosville@users.noreply.github.com>
2025-09-21 17:51:59 -0400

    Update route.ts

--- app/api/analytics/route.ts
+++ app/api/analytics/route.ts
@@ -1,6 +1,7 @@
 // app/api/analytics/route.ts
 import { NextResponse } from 'next/server';
 import { createClient } from '@supabase/supabase-js';
+import { Listing } from '../../../types/listings';
 
 const supabaseUrl = process.env.SUPABASE_URL!;
 const supabaseKey = process.env.SUPABASE_ANON_KEY!;
