8cd29de7a1d3cf2803a48209beb43e2458fc142b
jeffsosville <148246966+jeffsosville@users.noreply.github.com>
2025-09-22 17:41:22 -0400

    Update daily_scraper.py

--- daily_scraper.py
+++ daily_scraper.py
@@ -163,8 +163,9 @@ class DatabaseManager:
 
             try:
                 self.client.table("daily_listings").upsert(
-                cleaned_batch, on_conflict="listNumber"
+                cleaned_batch, on_conflict=["listNumber", "surrogate_key"]
                 ).execute()
+
 
                 ).execute()
                 total_inserted += len(cleaned_batch)
