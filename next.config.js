2e0391ea3665710dc388a81c0ba6bdab4bde8a11
jeffsosville <31511476+jasosville@users.noreply.github.com>
2025-10-31 12:03:45 -0400

    Add programmatic SEO pages

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/sitemap.xml',
+        destination: '/api/sitemap.xml',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
